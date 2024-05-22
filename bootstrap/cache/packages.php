266e5e00d1522cadf8836f8cced69822545ea8fa
hralamin <hralamin2020@gmail.com>
2024-05-22 14:40:33 +0600

    123

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -20,6 +20,17 @@
       0 => 'BladeUI\\Icons\\BladeIconsServiceProvider',
     ),
   ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
   'jantinnerezo/livewire-alert' => 
   array (
     'providers' => 
